0a08e2b690e44a97d95513754a60c88dd97d1a46
Janderson Araujo <j.a.brito@live.com>
2015-10-04 16:33:44 -0300

    no message

--- main.lua
+++ main.lua
@@ -8,8 +8,9 @@ io.write("Bem vindos ao RPG Game\n")
 io.write("Aperte Q para sair\n")
 
 while(quit == false) do
-		explore(currentRoom)
+
 		a = io.read();
+		explore(currentRoom)
 		if(a == "q")then
 		quit = true
 		end
@@ -19,7 +20,8 @@ end
 
 
 function explore(Room)
-
+ClearCreen()
+io.write("escrevendo alguma coisa")
 end
 
 function main()
@@ -28,6 +30,13 @@ iniciar()
 end
 
 
+function ClearCreen()
+
+for var=0,50 do
+	io.write("\n");
+    end
+
+end
 
 
 

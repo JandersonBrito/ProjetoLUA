4171da4b528e52e4d56639d5b0ff803dad186518
Janderson Araujo <j.a.brito@live.com>
2015-10-04 01:00:03 -0300

    arqivo da floresta

---
+++ floresta.lua
@@ -0,0 +1,12 @@
+floresta={
+
+};
+
+function onEnter()
+io.write("Você entrou na floresta")
+end
+
+floresta.nome = "Floresta Negra"
+floresta.descricao = "Você entrou na floresta negra... ela é um pouco assustadora"
+
+floresta.onEnter = onEnter;

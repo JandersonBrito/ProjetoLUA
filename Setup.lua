94a54cf328796d1211bd102986fed54173c30255
Janderson Araujo <j.a.brito@live.com>
2015-10-04 02:44:13 -0300

    no message

--- Setup.lua
+++ Setup.lua
@@ -1,3 +1,10 @@
-start = CreateRoom("floresta")
-SetStartRoom(start)
-AddNPCToRoom(start,CreatNPC("Knigth"));
+---
+--- Setup Script
+---
+load_assembly("RPGLUA");
+NPC = import_type("RPGLUA.NPC");
+start = CreateRoom("forest");
+SetStartRoom(start);
+npc = CreateNPC("knight");
+knight.this = npc;
+AddNPCToRoom(start, npc);

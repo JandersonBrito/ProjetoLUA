1bd65867e0ecb0c3034fd4cd573c32b7f8900215
Janderson Araujo <j.a.brito@live.com>
2015-10-15 22:03:51 -0300

    no message

--- Setup.lua
+++ Setup.lua
@@ -1,8 +1,8 @@
 ---
 --- Setup Script
 ---
-load_assembly("RPGLUA");
-NPC = import_type("RPGLUA.NPC");
+--load_assembly("RPGLUA");
+--NPC = import_type("RPGLUA.NPC");
 start = CreateRoom("forest");
 SetStartRoom(start);
 npc = CreateNPC("knight");
